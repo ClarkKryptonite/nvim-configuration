dc1918b07ea0f742daa48fe8ab03a12fd4f3993f
ClarkKryptonite <2742649263@qq.com>
2022-04-04 23:10:55 +0800

    Add: impatient plugin to speed up loading lua modules

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,26 +1,27 @@
-return require'packer'.startup(function()
-    use 'wbthomason/packer.nvim'
-    use 'kyazdani42/nvim-web-devicons' -- enable icons
-    use 'EdenEast/nightfox.nvim'
-    use 'kyazdani42/nvim-tree.lua' -- file explorer
-    use 'rcarriga/nvim-notify'
-    use 'nvim-lualine/lualine.nvim' -- a statusline sritten in lua
-    use 'romgrk/barbar.nvim' -- tabs for neovim
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-    use 'nvim-treesitter/nvim-treesitter-refactor'
-    use {
-        'nvim-telescope/telescope.nvim', -- Find, Filter, Preview, Pick. All lua, all the time.
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use 'windwp/nvim-autopairs'
+return require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+	use("kyazdani42/nvim-web-devicons") -- enable icons
+	use("EdenEast/nightfox.nvim")
+	use("kyazdani42/nvim-tree.lua") -- file explorer
+	use("rcarriga/nvim-notify")
+	use("nvim-lualine/lualine.nvim") -- a statusline sritten in lua
+	use("romgrk/barbar.nvim") -- tabs for neovim
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("nvim-treesitter/nvim-treesitter-refactor")
+	use({
+		"nvim-telescope/telescope.nvim", -- Find, Filter, Preview, Pick. All lua, all the time.
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("windwp/nvim-autopairs")
+	use("lewis6991/impatient.nvim") -- Speed up loading Lua modules in Neovim to improve startup time.
 
-    -- LSP plugins
-    use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
-    use 'williamboman/nvim-lsp-installer' -- Companion plugin for lsp-config, allows us to seamlesly install language servers
-    use 'jose-elias-alvarez/null-ls.nvim' --> inject lsp diagnistocs, formattings, code actions, and more ...
-    use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-    use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-    use 'L3MON4D3/LuaSnip' -- Snippets plugin
-    use 'onsails/lspkind-nvim' -- vscode-like pictograms for neovim lsp completion items
+	-- LSP plugins
+	use("neovim/nvim-lspconfig") -- Collection of configurations for built-in LSP client
+	use("williamboman/nvim-lsp-installer") -- Companion plugin for lsp-config, allows us to seamlesly install language servers
+	use("jose-elias-alvarez/null-ls.nvim") --> inject lsp diagnistocs, formattings, code actions, and more ...
+	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
+	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+	use("L3MON4D3/LuaSnip") -- Snippets plugin
+	use("onsails/lspkind-nvim") -- vscode-like pictograms for neovim lsp completion items
 end)
