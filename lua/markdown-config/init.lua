695afdfeb457c61449f1b4899d6c7fafe708e722
kun <kun.chen@qizhidao.com>
2022-09-22 16:07:57 +0800

    Modify(markdown): add snippet

--- lua/markdown-config/init.lua
+++ lua/markdown-config/init.lua
@@ -1,4 +1,29 @@
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
-map("n", "<leader>mt", ":MarkdownPreviewToggle<CR>", opts)
+local cmd = vim.api.nvim_create_autocmd
+
+map("n", "<leader>mt", "<cmd>MarkdownPreviewToggle<CR>", opts)
+
+cmd("FileType", {
+	pattern = "markdown",
+	group = vim.api.nvim_create_augroup("MarkdownSnippet", { clear = true }),
+	callback = function()
+		map("i", ",t", "<cmd>MarkdownPreviewToggle<CR>", opts)
+		map("i", ",f", "<Esc>/<++><CR>:nohlsearch<CR>c4l", opts)
+		map("i", ",n", "---<Enter><Enter>", opts)
+		map("i", ",b", "**** <++><ESC>6hi", opts)
+		map("i", ",s", "~~~~ <++><ESC>6hi", opts)
+		map("i", ",i", "** <++><ESC>5hi", opts)
+		map("i", ",d", "`` <++><ESC>5hi", opts)
+		map("i", ",c", "```<Enter><++><Enter>```<Enter><Enter><++><Esc>4kA", opts)
+		map("i", ",h", "====<Space><++><ESC>7hi", opts)
+		map("i", ",p", "![](<++>) <++><Esc>11hi", opts)
+		map("i", ",a", "[](<++>) <++><Esc>11hi", opts)
+		map("i", ",1", "#<Space><Enter><++><Esc>kA", opts)
+		map("i", ",2", "##<Space><Enter><++><Esc>kA", opts)
+		map("i", ",3", "###<Space><Enter><++><Esc>kA", opts)
+		map("i", ",4", "####<Space><Enter><++><Esc>kA", opts)
+		map("i", ",l", "--------<Enter>", opts)
+	end,
+})
