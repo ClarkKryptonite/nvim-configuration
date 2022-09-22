e3976e5c25616e6316782bffa2972cc92879b2b0
kun <kun.chen@qizhidao.com>
2022-09-22 14:06:30 +0800

    Modify(hop): modify default 'f,F,t,T' key

--- lua/hop-config/init.lua
+++ lua/hop-config/init.lua
@@ -5,8 +5,20 @@ local map = vim.api.nvim_set_keymap
 -- place this in one of your configuration file(s)
 map(
 	"",
+	"f",
+	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
+	{}
+)
+map(
+	"",
 	"<leader><leader>f",
 	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR})<cr>",
+	{}
+)
+map(
+	"",
+	"F",
+	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
 	{}
 )
 map(
@@ -17,8 +29,20 @@ map(
 )
 map(
 	"",
+	"t",
+	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, hint_offset = -1, current_line_only = true })<cr>",
+	{}
+)
+map(
+	"",
 	"<leader><leader>t",
 	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, hint_offset = -1 })<cr>",
+	{}
+)
+map(
+	"",
+	"T",
+	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, hint_offset = 1 , current_line_only = true })<cr>",
 	{}
 )
 map(
