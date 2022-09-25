be568e254d5d8ded1da0df694cd79a2c7109303d
ClarkKryptonite <2742649263@qq.com>
2022-09-25 12:52:38 +0800

    Modify(code): lua code formatting

--- lua/hop-config/init.lua
+++ lua/hop-config/init.lua
@@ -1,53 +1,35 @@
 require("hop").setup({
-	case_insensitive = true,
+    case_insensitive = true,
 })
 local map = vim.api.nvim_set_keymap
 -- place this in one of your configuration file(s)
-map(
-	"",
-	"f",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>",
-	{}
+map("", "f",
+    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>"
+    , {}
 )
-map(
-	"",
-	"<leader><leader>f",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR})<cr>",
-	{}
+map("", "<leader><leader>f",
+    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR})<cr>", {}
 )
-map(
-	"",
-	"F",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>",
-	{}
+map("", "F",
+    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>"
+    , {}
 )
-map(
-	"",
-	"<leader><leader>F",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR})<cr>",
-	{}
+map("", "<leader><leader>F",
+    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR})<cr>", {}
 )
-map(
-	"",
-	"t",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, hint_offset = -1, current_line_only = true })<cr>",
-	{}
+map("", "t",
+    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, hint_offset = -1, current_line_only = true })<cr>"
+    , {}
 )
-map(
-	"",
-	"<leader><leader>t",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, hint_offset = -1 })<cr>",
-	{}
+map("", "<leader><leader>t",
+    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, hint_offset = -1 })<cr>"
+    , {}
 )
-map(
-	"",
-	"T",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, hint_offset = 1 , current_line_only = true })<cr>",
-	{}
+map("", "T",
+    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, hint_offset = 1 , current_line_only = true })<cr>"
+    , {}
 )
-map(
-	"",
-	"<leader><leader>T",
-	"<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, hint_offset = 1 })<cr>",
-	{}
+map("", "<leader><leader>T",
+    "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, hint_offset = 1 })<cr>"
+    , {}
 )
--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,51 +1,51 @@
 return require("packer").startup(function()
-	use("wbthomason/packer.nvim")
-	use("kyazdani42/nvim-web-devicons") -- enable icons
-	use("EdenEast/nightfox.nvim")
-	use("kyazdani42/nvim-tree.lua") -- file explorer
-	use("rcarriga/nvim-notify")
-	use("nvim-lualine/lualine.nvim") -- a statusline sritten in lua
-	use("romgrk/barbar.nvim") -- tabs for neovim
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use("nvim-treesitter/nvim-treesitter-refactor")
-	use("nvim-lua/plenary.nvim") -- require by telescope and neovim-session-manager
-	use("nvim-telescope/telescope.nvim") -- Find, Filter, Preview, Pick. All lua, all the time.
-	use("windwp/nvim-autopairs")
-	use("lewis6991/impatient.nvim") -- Speed up loading Lua modules in Neovim to improve startup time.
-	use("Shatur/neovim-session-manager") -- Manage sessions like folders in VSCode
+    use("wbthomason/packer.nvim")
+    use("kyazdani42/nvim-web-devicons") -- enable icons
+    use("EdenEast/nightfox.nvim")
+    use("kyazdani42/nvim-tree.lua") -- file explorer
+    use("rcarriga/nvim-notify")
+    use("nvim-lualine/lualine.nvim") -- a statusline sritten in lua
+    use("romgrk/barbar.nvim") -- tabs for neovim
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+    use("nvim-treesitter/nvim-treesitter-refactor")
+    use("nvim-lua/plenary.nvim") -- require by telescope and neovim-session-manager
+    use("nvim-telescope/telescope.nvim") -- Find, Filter, Preview, Pick. All lua, all the time.
+    use("windwp/nvim-autopairs")
+    use("lewis6991/impatient.nvim") -- Speed up loading Lua modules in Neovim to improve startup time.
+    use("Shatur/neovim-session-manager") -- Manage sessions like folders in VSCode
 
-	-- LSP plugins
-	use("neovim/nvim-lspconfig") -- Collection of configurations for built-in LSP client
-	use("williamboman/nvim-lsp-installer") -- Companion plugin for lsp-config, allows us to seamlesly install language servers
-	use("jose-elias-alvarez/null-ls.nvim") --> inject lsp diagnistocs, formattings, code actions, and more ...
-	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
-	use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
-	use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
-	use("L3MON4D3/LuaSnip") -- Snippets plugin
-	use("onsails/lspkind-nvim") -- vscode-like pictograms for neovim lsp completion items
+    -- LSP plugins
+    use("neovim/nvim-lspconfig") -- Collection of configurations for built-in LSP client
+    use("williamboman/nvim-lsp-installer") -- Companion plugin for lsp-config, allows us to seamlesly install language servers
+    use("jose-elias-alvarez/null-ls.nvim") --> inject lsp diagnistocs, formattings, code actions, and more ...
+    use("hrsh7th/nvim-cmp") -- Autocompletion plugin
+    use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
+    use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
+    use("L3MON4D3/LuaSnip") -- Snippets plugin
+    use("onsails/lspkind-nvim") -- vscode-like pictograms for neovim lsp completion items
 
-	-- tpope's plugins that should be part of vim
-	use({ "tpope/vim-surround" })
-	use({ "tpope/vim-repeat" })
-	use({ "tpope/vim-commentary" })
+    -- tpope's plugins that should be part of vim
+    use({ "tpope/vim-surround" })
+    use({ "tpope/vim-repeat" })
+    use({ "tpope/vim-commentary" })
 
-	-- markdown
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	})
-	use({ "dhruvasagar/vim-table-mode" })
-	use({ "mzlogin/vim-markdown-toc" })
-	use({ "ferrine/md-img-paste.vim" })
+    -- markdown
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    })
+    use({ "dhruvasagar/vim-table-mode" })
+    use({ "mzlogin/vim-markdown-toc" })
+    use({ "ferrine/md-img-paste.vim" })
 
-	-- git
-	use("lewis6991/gitsigns.nvim")
+    -- git
+    use("lewis6991/gitsigns.nvim")
 
-	-- nvim motions on speed
-	use({
-		"phaazon/hop.nvim",
-		branch = "v2", -- optional but strongly recommended
-	})
+    -- nvim motions on speed
+    use({
+        "phaazon/hop.nvim",
+        branch = "v2", -- optional but strongly recommended
+    })
 end)
