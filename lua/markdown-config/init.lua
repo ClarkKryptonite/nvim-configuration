6afb3bc12d0a52db966d872985cbe8881c695e99
ClarkKryptonite <2742649263@qq.com>
2022-10-15 20:21:13 +0800

    Modify(keymap): markdown kemap

--- lua/markdown-config/init.lua
+++ lua/markdown-config/init.lua
@@ -9,33 +9,33 @@ vim.g.mkdp_open_to_the_world = 0
 vim.g.mkdp_theme = "light"
 
 cmd("FileType", {
-	pattern = "markdown",
-	group = vim.api.nvim_create_augroup("MarkdownSnippet", { clear = true }),
-	callback = function()
-		-- enbale table snippet
-		vim.api.nvim_exec("TableModeEnable", true)
-		-- normal mode keymap
-		map("n", "<leader>tc", "<cmd>GenTocMarked<CR>", opts)
-		-- insert mode keymap
-		-- function
-		map("i", ",o", "<cmd>MarkdownPreview<CR>", opts)
-		map("i", ",O", "<cmd>MarkdownPreviewStop<CR>", opts)
-		map("i", ",P", "<cmd>call mdip#MarkdownClipboardImage()<CR><Esc><Esc>A", opts)
-		-- snippet
-		map("i", ",f", "<Esc>/<++><CR>:nohlsearch<CR>c4l", opts)
-		map("i", ",n", "---<Enter><Enter>", opts)
-		map("i", ",b", "**** <++><ESC>6hi", opts)
-		map("i", ",s", "~~~~ <++><ESC>6hi", opts)
-		map("i", ",i", "** <++><ESC>5hi", opts)
-		map("i", ",d", "`` <++><ESC>5hi", opts)
-		map("i", ",c", "```<Enter><++><Enter>```<Enter><Enter><++><Esc>4kA", opts)
-		map("i", ",h", "====<Space><++><ESC>7hi", opts)
-		map("i", ",p", "![](<++>) <++><Esc>11hi", opts)
-		map("i", ",a", "[](<++>) <++><Esc>11hi", opts)
-		map("i", ",1", "#<Space><Enter><++><Esc>kA", opts)
-		map("i", ",2", "##<Space><Enter><++><Esc>kA", opts)
-		map("i", ",3", "###<Space><Enter><++><Esc>kA", opts)
-		map("i", ",4", "####<Space><Enter><++><Esc>kA", opts)
-		map("i", ",l", "--------<Enter>", opts)
-	end,
+    pattern = "markdown",
+    group = vim.api.nvim_create_augroup("MarkdownSnippet", { clear = true }),
+    callback = function()
+        -- enbale table snippet
+        vim.api.nvim_exec("TableModeEnable", true)
+        -- normal mode keymap
+        map("n", "<leader>tc", "<cmd>GenTocMarked<CR>", opts)
+        -- insert mode keymap
+        -- function
+        map("i", ";o", "<cmd>MarkdownPreview<CR>", opts)
+        map("i", ";O", "<cmd>MarkdownPreviewStop<CR>", opts)
+        map("i", ";P", "<cmd>call mdip#MarkdownClipboardImage()<CR><Esc><Esc>A", opts)
+        -- snippet
+        map("i", ";f", "<Esc>/<++><CR>:nohlsearch<CR>c4l", opts)
+        map("i", ";n", "---<Enter><Enter>", opts)
+        map("i", ";b", "**** <++><ESC>6hi", opts)
+        map("i", ";s", "~~~~ <++><ESC>6hi", opts)
+        map("i", ";i", "** <++><ESC>5hi", opts)
+        map("i", ";c", "`` <++><ESC>5hi", opts)
+        map("i", ";C", "```<Enter><++><Enter>```<Enter><Enter><++><Esc>4kA", opts)
+        map("i", ";h", "====<Space><++><ESC>7hi", opts)
+        map("i", ";p", "![](<++>) <++><Esc>11hi", opts)
+        map("i", ";a", "[](<++>) <++><Esc>11hi", opts)
+        map("i", ";1", "#<Space><Enter><++><Esc>kA", opts)
+        map("i", ";2", "##<Space><Enter><++><Esc>kA", opts)
+        map("i", ";3", "###<Space><Enter><++><Esc>kA", opts)
+        map("i", ";4", "####<Space><Enter><++><Esc>kA", opts)
+        map("i", ";l", "--------<Enter>", opts)
+    end,
 })
