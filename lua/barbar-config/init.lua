b2c2749b3751c16e3085f8671c5a7c45aaf3e079
ClarkKryptonite <2742649263@qq.com>
2023-04-16 11:50:03 +0800

    Modify(barbar): setting deprecated change

--- lua/barbar-config/init.lua
+++ lua/barbar-config/init.lua
@@ -1,9 +1,35 @@
-vim.g.bufferline = {
-    -- Enable/disable animations
-    animation = true,
-    -- Enable/disable close button
-    closable = true,
-    -- Configure icons on the bufferline.
-    icon_separator_active = '',
-    icon_separator_inactive = '',
-}
+vim.g.barbar_auto_setup = false
+require("barbar").setup({
+	animation = true,
+	icons = {
+		-- Configure the base icons on the bufferline.
+		buffer_index = false,
+		buffer_number = false,
+		button = "",
+		-- Enables / disables diagnostic symbols
+		diagnostics = {
+			[vim.diagnostic.severity.ERROR] = { enabled = true, icon = "ﬀ" },
+			[vim.diagnostic.severity.WARN] = { enabled = false },
+			[vim.diagnostic.severity.INFO] = { enabled = false },
+			[vim.diagnostic.severity.HINT] = { enabled = true },
+		},
+		filetype = {
+			-- Sets the icon's highlight group.
+			-- If false, will use nvim-web-devicons colors
+			custom_colors = false,
+			-- Requires `nvim-web-devicons` if `true`
+			enabled = true,
+		},
+		separator = { left = "▎", right = "" },
+		-- Configure the icons on the bufferline when modified or pinned.
+		-- Supports all the base icon options.
+		modified = { button = "●" },
+		pinned = { button = "車", filename = true, separator = { right = "" } },
+		-- Configure the icons on the bufferline based on the visibility of a buffer.
+		-- Supports all the base icon options, plus `modified` and `pinned`.
+		alternate = { filetype = { enabled = false } },
+		current = { buffer_index = true },
+		inactive = { button = "×" },
+		visible = { modified = { buffer_number = false } },
+	},
+})
