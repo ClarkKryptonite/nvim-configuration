004ebb02adbeaa63f2ec42158eda55b44252cafa
kun <kun.chen@qizhidao.com>
2025-05-12 14:40:44 +0800

    Modify(mason-lsp): remove deprecated config

--- lua/lsp-config/mason-lspconfig.lua
+++ lua/lsp-config/mason-lspconfig.lua
@@ -51,10 +51,10 @@ local common_on_attach = function(client, bufnr)
     end, bufopts)
 end
 
-require("mason-lspconfig").setup_handlers({
-    function(server_name)
-        require("lspconfig")[server_name].setup({
-            on_attach = common_on_attach,
-        })
-    end,
-})
+-- require("mason-lspconfig").setup_handlers({
+--     function(server_name)
+--         require("lspconfig")[server_name].setup({
+--             on_attach = common_on_attach,
+--         })
+--     end,
+-- })
